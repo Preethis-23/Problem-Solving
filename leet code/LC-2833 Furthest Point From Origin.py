4937e1fb6a124230bf26be808c4479fca9550c48
Preethis-23 <preethisff@gmail.com>
2026-04-24 23:16:20 +0530

    done!

---
+++ leet code/LC-2833 Furthest Point From Origin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        l, r, unknown = 0, 0, 0
+        for i in range(len(moves)):
+            if moves[i] == "L":
+                l += 1
+            elif moves[i] == "R":
+                r += 1
+            else:
+                unknown += 1
+
+        max_distance = max(l, r) - min(l, r) + unknown
+        return max_distance
