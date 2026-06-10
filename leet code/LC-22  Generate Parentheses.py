cc3a3e75b971606a3d3d4bc6c09374fe2405af3a
Preethis-23 <preethisff@gmail.com>
2026-06-10 15:55:56 +0530

    done!

---
+++ leet code/LC-22  Generate Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtrack(open, close, string):
+            nonlocal result
+
+            if len(string) == n * 2:
+                result.append(string)
+                return
+
+            if open < n:
+                backtrack(open + 1, close, string + '(')
+            if close < open:
+                backtrack(open, close + 1, string + ')')
+            
+        backtrack(0, 0, '')
+        return result
